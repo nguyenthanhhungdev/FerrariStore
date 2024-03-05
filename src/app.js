f61649e3f2b7b700714440c67f87eed1aad0dc82
hungnguyen <nguyenthanhhung.dev@gmail.com>
2024-03-05 20:20:07 +0700

    Vs k biết Commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+/**
+ * Sử dụng require để import module express
+ * Sau đó tạo một thể hiện của express bằng cách gọi hàm express().
+ */
+const express = require('express')
+const app = express()
+
+// Init middleware
+
+
+/**
+ * Sau khi đã import module vào bằng câu lệnh require thì ta thực hiện exports module
+ */
+module.exports = app
