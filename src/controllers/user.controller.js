c5e9c44d371fd0b1665b62b34f28aa9b442b7467
nguyenthanhhung <nguyenthanhhung.dev@gmail.com>
2024-10-07 17:58:27 +0700

    Cập nhật tên userService trong controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,28 +1,30 @@
-const { signUp } = require('../services/user.service');
+const userService = require('../services/user.service');
 
-const signupController = async (req, res) => {
-    try {
-        const { token,refreshToken, user } = await signUp(req.body);
-        // Set the refresh token as an HTTP-only cookie
-        res.cookie("refreshToken", refreshToken, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
-        });
+class UserController {
+    signupController = async (req, res) => {
+        try {
+            const {token, refreshToken, user} = await userService.signUp(req.body);
+            // Set the refresh token as an HTTP-only cookie
+            res.cookie("refreshToken", refreshToken, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: "strict",
+            });
 
-        res.status(200).json({
-            success: true,
-            user,
-            token: `Bearer ${token}`
-        });
+            res.status(200).json({
+                success: true,
+                user,
+                token: `Bearer ${token}`
+            });
 
 
-    } catch (error) {
-        res.status(error.statusCode || 500).json({
-            success: false,
-            message: error.message
-        });
-    }
-};
+        } catch (error) {
+            res.status(error.statusCode || 500).json({
+                success: false,
+                error: error.message
+            });
+        }
+    };
+}
 
-module.exports = { signupController };
+module.exports = new UserController();
