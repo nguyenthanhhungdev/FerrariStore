dc78c7431519d39bedf157b25b27ab6e56a4f68f
nguyenthanhhung <nguyenthanhhung.dev@gmail.com>
2024-10-07 17:59:49 +0700

    Thêm xác thực trong service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,41 +2,36 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { User } = require('../models/user.model');
 
-const validateUserData = (userData) => {
-    if (!userData.email) throw new Error('You must enter an email address.');
-    if (!userData.name) throw new Error('You must enter your full name.');
-    if (!userData.password) throw new Error('You must enter a password.');
-    if (!userData.phone) throw new Error('You must enter a phone number.');
-};
+class UserService {
+    signUp = async (userData) => {
+        try {
+            const existingUser = await User.findOne({email: userData.email});
+            if (existingUser) throw new Error('User already exists');
 
-const signUp = async (userData) => {
-    try {
-        validateUserData(userData);
+            const hashedPassword = await bcrypt.hash(userData.password, 10);
+            const user = new User({...userData, password: hashedPassword});
+            const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: '15m'});
 
-        const existingUser = await User.findOne({email: userData.email});
-        if (existingUser) throw new Error('User already exists');
+            const refreshToken = jwt.sign({userId: user._id}, process.env.JWT_REFRESH_SECRET, {expiresIn: '7d'});
 
-        const hashedPassword = await bcrypt.hash(userData.password, 10);
-        const user = new User({...userData, password: hashedPassword});
-        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: '15m'});
+            user.refreshToken = refreshToken; // Lưu refreshToken vào cơ sở dữ liệu
 
-        const refreshToken = jwt.sign({userId: user._id}, process.env.JWT_REFRESH_SECRET, {expiresIn: '7d'});
+            await user.save();
 
-        await user.save();
-
-        return {
-            token: token,
-            refreshToken: refreshToken,
-            user: {
-                id: user._id,
-                name: user.name,
-                email: user.email,
-                role: user.role
-            }
-        };
-    } catch (error) {
-        console.log(":::E::: Error in service: ",error);
-        throw error;
-    }};
-
-module.exports = { signUp };
+            return {
+                token: token,
+                refreshToken: refreshToken,
+                user: {
+                    id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    role: user.role
+                }
+            };
+        } catch (error) {
+            console.log(":::E::: Error in user service: ", error);
+            throw error;
+        }
+    };
+}
+module.exports = new UserService();
